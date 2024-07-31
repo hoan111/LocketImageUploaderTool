d82ff83bd1a4c1014bc9dafbeee1f9bc1eaffbb8
hoannk <hoan489@gmail.com>
2024-07-31 13:18:43 +0700

    Init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+using Firebase.Auth;
+using Firebase.Storage;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace LocketImageUploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UserNameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.LogoutBtn = new System.Windows.Forms.Button();
+            this.UploadProgessBar = new System.Windows.Forms.ProgressBar();
+            this.LinkTextBox = new System.Windows.Forms.TextBox();
+            this.BtnChooseFile = new System.Windows.Forms.Button();
+            this.TxtBoxFilePath = new System.Windows.Forms.TextBox();
+            this.CheckBoxUploadVideo = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // UserNameTextBox
+            // 
+            this.UserNameTextBox.Location = new System.Drawing.Point(75, 9);
+            this.UserNameTextBox.Name = "UserNameTextBox";
+            this.UserNameTextBox.Size = new System.Drawing.Size(192, 20);
+            this.UserNameTextBox.TabIndex = 2;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(75, 42);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(192, 20);
+            this.PasswordTextBox.TabIndex = 3;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Location = new System.Drawing.Point(302, 13);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(79, 42);
+            this.LoginBtn.TabIndex = 4;
+            this.LoginBtn.Text = "Đăng nhập";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LogoutBtn
+            // 
+            this.LogoutBtn.Enabled = false;
+            this.LogoutBtn.Location = new System.Drawing.Point(387, 13);
+            this.LogoutBtn.Name = "LogoutBtn";
+            this.LogoutBtn.Size = new System.Drawing.Size(79, 42);
+            this.LogoutBtn.TabIndex = 6;
+            this.LogoutBtn.Text = "Đăng xuất";
+            this.LogoutBtn.UseVisualStyleBackColor = true;
+            this.LogoutBtn.Click += new System.EventHandler(this.LogoutBtn_Click);
+            // 
+            // UploadProgessBar
+            // 
+            this.UploadProgessBar.Location = new System.Drawing.Point(11, 112);
+            this.UploadProgessBar.Name = "UploadProgessBar";
+            this.UploadProgessBar.Size = new System.Drawing.Size(529, 23);
+            this.UploadProgessBar.TabIndex = 7;
+            // 
+            // LinkTextBox
+            // 
+            this.LinkTextBox.Location = new System.Drawing.Point(12, 152);
+            this.LinkTextBox.Multiline = true;
+            this.LinkTextBox.Name = "LinkTextBox";
+            this.LinkTextBox.ReadOnly = true;
+            this.LinkTextBox.Size = new System.Drawing.Size(528, 85);
+            this.LinkTextBox.TabIndex = 8;
+            // 
+            // BtnChooseFile
+            // 
+            this.BtnChooseFile.Location = new System.Drawing.Point(16, 78);
+            this.BtnChooseFile.Name = "BtnChooseFile";
+            this.BtnChooseFile.Size = new System.Drawing.Size(75, 23);
+            this.BtnChooseFile.TabIndex = 9;
+            this.BtnChooseFile.Text = "Chọn file";
+            this.BtnChooseFile.UseVisualStyleBackColor = true;
+            this.BtnChooseFile.Click += new System.EventHandler(this.BtnChooseFile_Click);
+            // 
+            // TxtBoxFilePath
+            // 
+            this.TxtBoxFilePath.Location = new System.Drawing.Point(110, 78);
+            this.TxtBoxFilePath.Name = "TxtBoxFilePath";
+            this.TxtBoxFilePath.ReadOnly = true;
+            this.TxtBoxFilePath.Size = new System.Drawing.Size(356, 20);
+            this.TxtBoxFilePath.TabIndex = 10;
+            // 
+            // CheckBoxUploadVideo
+            // 
+            this.CheckBoxUploadVideo.AutoSize = true;
+            this.CheckBoxUploadVideo.Location = new System.Drawing.Point(473, 83);
+            this.CheckBoxUploadVideo.Name = "CheckBoxUploadVideo";
+            this.CheckBoxUploadVideo.Size = new System.Drawing.Size(89, 17);
+            this.CheckBoxUploadVideo.TabIndex = 11;
+            this.CheckBoxUploadVideo.Text = "Upload video";
+            this.CheckBoxUploadVideo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 313);
+            this.Controls.Add(this.CheckBoxUploadVideo);
+            this.Controls.Add(this.TxtBoxFilePath);
+            this.Controls.Add(this.BtnChooseFile);
+            this.Controls.Add(this.LinkTextBox);
+            this.Controls.Add(this.UploadProgessBar);
+            this.Controls.Add(this.LogoutBtn);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UserNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Locket Image Upload Tool By HoanNK";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UserNameTextBox;
+        private System.Windows.Forms.MaskedTextBox PasswordTextBox;
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.Button LogoutBtn;
+        private System.Windows.Forms.ProgressBar UploadProgessBar;
+        private System.Windows.Forms.TextBox LinkTextBox;
+        private System.Windows.Forms.Button BtnChooseFile;
+        private System.Windows.Forms.TextBox TxtBoxFilePath;
+        private System.Windows.Forms.CheckBox CheckBoxUploadVideo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+using Firebase.Auth;
+using Firebase.Storage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices.ComTypes;
+using Imazen.WebP;
+using System.CodeDom;
+
+namespace LocketImageUploader
+{
+    public partial class Form1 : Form
+    {
+        private string Username {  get; set; }
+        private string Password { get; set; }
+
+        private static FirebaseAuthClient FirebaseAuthClient = null;
+        private static UserCredential UserCredential = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Username = UserNameTextBox.Text;
+            Password = PasswordTextBox.Text;
+            LoginToFireBase(Username, Password);
+        }
+
+        public async void LoginToFireBase(string username, string password)
+        {
+            if(String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password)) 
+            {
+                MessageBox.Show("Username hoặc Password không được bỏ trống", "Lỗi", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; 
+            }
+            var imageUrl = "";
+            var videoUrl = "";
+            try
+            {
+                var authConfig = new FirebaseAuthConfig
+                {
+                    ApiKey = "AIzaSyCQngaaXQIfJaH0aS2l7REgIjD7nL431So",
+                    AuthDomain = "locket-4252a.firebaseapp.com",
+                    Providers = new FirebaseAuthProvider[]
+{
+                            new EmailProvider(),
+},
+                    UserRepository = new FileUserRepository("FirebaseRepo"), // Có thể được sử dụng để lưu thông tin login
+                };
+                FirebaseAuthClient = new FirebaseAuthClient(authConfig);
+                UserCredential = await FirebaseAuthClient.SignInWithEmailAndPasswordAsync(username, password);
+                var user = UserCredential.User;
+                var token = await user.GetIdTokenAsync();
+
+                using (var stream = File.Open(TxtBoxFilePath.Text, FileMode.Open))
+                {
+
+                    var task = new FirebaseStorage("locket-img", new FirebaseStorageOptions
+                    {
+                        AuthTokenAsyncFactory = () => Task.FromResult(token), //Token = IdToken trên locket
+                        ThrowOnCancel = true,
+
+                    })
+                    .Child("users")
+                    .Child(user.Uid) //User ID trên locket
+                    .Child("moments")
+                    .Child("thumbnails")
+                    .Child($"{System.Guid.NewGuid().ToString().Replace("-", "").Substring(0, 21)}.webp")
+                    .PutAsync(stream);
+
+                    //task.Progress.ProgressChanged += (s, e) => UploadProgessBar.Value = e.Percentage;
+                    imageUrl = await task;
+
+                    //TODO: call locket function
+                    string FirebaseFunctions = "api.locketcamera.com";
+                    string FirebaseCallFunction = "postMomentV2";
+                    ChangeButtonStateAfterLogin();
+                }
+
+                bool uploadVideo = CheckBoxUploadVideo.Checked;
+                if(uploadVideo)
+                {
+                    using (var streamVideo = File.Open(TxtBoxFilePath.Text, FileMode.Open))
+                    {
+                        var task = new FirebaseStorage("locket-video", new FirebaseStorageOptions
+                        {
+                            AuthTokenAsyncFactory = () => Task.FromResult(token), //Token = IdToken trên locket
+                            ThrowOnCancel = true,
+
+                        })
+                        .Child("users")
+                        .Child(user.Uid) //User ID trên locket
+                        .Child("moments")
+                        .Child("videos")
+                        .Child($"{System.Guid.NewGuid().ToString().Replace("-", "").Substring(0, 21)}.mp4")
+                        .PutAsync(streamVideo);
+
+                        //task.Progress.ProgressChanged += (s, e) => UploadProgessBar.Value = e.Percentage;
+                        videoUrl = await task;
+                    }
+                }
+            } catch(Exception e)
+            {
+                MessageBox.Show(e.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            LinkTextBox.Text = $"ImageUrl: {imageUrl} ------ videoUrl: {videoUrl}";
+        }
+        private void ChangeButtonStateAfterLogin()
+        {
+            LoginBtn.Enabled = false;
+            LogoutBtn.Enabled = true;
+            UserNameTextBox.Text = string.Empty;
+            PasswordTextBox.Text = string.Empty;
+        }
+        private void ChangeButtonStateAfterLogout()
+        {
+            LoginBtn.Enabled = true;
+            LogoutBtn.Enabled = false;
+            LinkTextBox.Text = string.Empty;
+        }
+        private void LogoutBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FirebaseAuthClient.SignOut();
+                MessageBox.Show("Logout successfully");
+
+                FirebaseAuthClient = null;
+                UserCredential = null;
+                ChangeButtonStateAfterLogout();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public byte[] convertImageToWebpAsync(string path)
+        {
+            using (Bitmap bitmap = new Bitmap(path))
+            using (var outStream = new MemoryStream())
+            {
+                var encoder = new SimpleEncoder();
+                encoder.Encode(bitmap, outStream, 20);
+                return outStream.ToArray();
+            } 
+        }
+
+        private void BtnChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select an image";
+            openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog.ShowDialog();
+            if (String.IsNullOrEmpty(openFileDialog.FileName))
+            {
+                MessageBox.Show("Please select a file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                openFileDialog.ShowDialog();
+                return;
+            }
+            TxtBoxFilePath.Text = openFileDialog.FileName;
+        }
+    }
+}
